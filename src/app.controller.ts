d5d22086661c17f7ddb7c3da7ecf02c876876c92
aymensellaouti <aymen.sellaouti@gmail.com>
2026-02-06 17:37:32 +0100

    S2 seance du 6 2 20206

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,17 @@
-import { Controller, Get } from '@nestjs/common';
+import { Body, Controller, Get, Param, Post } from '@nestjs/common';
 import { AppService } from './app.service';
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  constructor() {}
 
-  @Get()
+  @Get('')
   getHello(): string {
-    return this.appService.getHello();
+    return 'Hello GL3';
+  }
+
+  @Post('')
+  setHello(@Body('test') body): string {
+    return 'Hello GL3';
   }
 }
