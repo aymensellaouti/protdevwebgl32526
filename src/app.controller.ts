4b56718c8500c532d5fa7b618e75da864004d6ca
aymensellaouti <aymen.sellaouti@gmail.com>
2026-02-16 08:57:13 +0000

    S3 providers

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,17 +1,25 @@
-import { Body, Controller, Get, Param, Post } from '@nestjs/common';
+import { Body, Controller, Get, Inject, Param, Post, Req } from '@nestjs/common';
 import { AppService } from './app.service';
+import { Request } from 'express';
 
-@Controller()
+@Controller('user')
 export class AppController {
-  constructor() {}
+  // HAw chneya nhab
+  constructor(@Inject(AppService) private appService: AppService) {}
 
   @Get('')
   getHello(): string {
     return 'Hello GL3';
   }
 
-  @Post('')
-  setHello(@Body('test') body): string {
-    return 'Hello GL3';
+  @Post(':id')
+  addSkillToUser(
+    @Body() skill,
+    @Param('id') id,
+    @Req() request: Request
+  ): string {
+   // console.log(request);
+    
+    return skill + id;
   }
 }
